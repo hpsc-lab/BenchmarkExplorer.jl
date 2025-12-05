0a11b59a99e8586cd70dcc8e5b7c05e240cb819a
Yaraskau Kachur <yaraslau.prog.j@gmail.com>
2025-12-05 22:11:00 +0100

    add enzyme ci
    Add separate CI job for Enzyme benchmarks and standardize suite naming.
    
    - Add benchmark-enzyme job to GitHub Actions workflow
    - Rename SUITE_ENZYME to SUITE in enzyme benchmarks for CI compatibility
    - Configure enzyme benchmarks to use data/history_enzyme.json
    - Set up parallel execution of Trixi and Enzyme benchmark jobs

--- benchmarks/enzyme/benchmarks.jl
+++ benchmarks/enzyme/benchmarks.jl
@@ -1,36 +1,36 @@
 using BenchmarkTools
 using Enzyme
 
-const SUITE_ENZYME = BenchmarkGroup()
+const SUITE = BenchmarkGroup()
 
-SUITE_ENZYME["basics"] = BenchmarkGroup()
-SUITE_ENZYME["basics"]["make_zero"] = BenchmarkGroup()
+SUITE["basics"] = BenchmarkGroup()
+SUITE["basics"]["make_zero"] = BenchmarkGroup()
 
 p_namedtuple = (a = 1.0, b = 2.0, c = 3.0)
-SUITE_ENZYME["basics"]["make_zero"]["namedtuple"] = @benchmarkable Enzyme.make_zero($p_namedtuple)
+SUITE["basics"]["make_zero"]["namedtuple"] = @benchmarkable Enzyme.make_zero($p_namedtuple)
 
 p_array = rand(100)
-SUITE_ENZYME["basics"]["make_zero"]["array"] = @benchmarkable Enzyme.make_zero($p_array)
+SUITE["basics"]["make_zero"]["array"] = @benchmarkable Enzyme.make_zero($p_array)
 
 struct MyStruct
     x::Float64
     y::Float64
 end
 p_struct = MyStruct(1.0, 2.0)
-SUITE_ENZYME["basics"]["make_zero"]["struct"] = @benchmarkable Enzyme.make_zero($p_struct)
+SUITE["basics"]["make_zero"]["struct"] = @benchmarkable Enzyme.make_zero($p_struct)
 
-SUITE_ENZYME["autodiff"] = BenchmarkGroup()
-SUITE_ENZYME["autodiff"]["forward"] = BenchmarkGroup()
-SUITE_ENZYME["autodiff"]["reverse"] = BenchmarkGroup()
+SUITE["autodiff"] = BenchmarkGroup()
+SUITE["autodiff"]["forward"] = BenchmarkGroup()
+SUITE["autodiff"]["reverse"] = BenchmarkGroup()
 
 f_simple(x) = sum(x .^ 2)
 x_simple = rand(100)
 
-SUITE_ENZYME["autodiff"]["forward"]["simple"] = @benchmarkable Enzyme.autodiff(
+SUITE["autodiff"]["forward"]["simple"] = @benchmarkable Enzyme.autodiff(
     Enzyme.Forward, $f_simple, Enzyme.Duplicated($x_simple, zero($x_simple))
 )
 
-SUITE_ENZYME["autodiff"]["reverse"]["simple"] = @benchmarkable Enzyme.autodiff(
+SUITE["autodiff"]["reverse"]["simple"] = @benchmarkable Enzyme.autodiff(
     Enzyme.Reverse, $f_simple, Enzyme.Active, Enzyme.Duplicated($x_simple, zero($x_simple))
 )
 
@@ -43,10 +43,10 @@ function f_complex(x)
 end
 x_complex = rand(1000)
 
-SUITE_ENZYME["autodiff"]["forward"]["complex"] = @benchmarkable Enzyme.autodiff(
+SUITE["autodiff"]["forward"]["complex"] = @benchmarkable Enzyme.autodiff(
     Enzyme.Forward, $f_complex, Enzyme.Duplicated($x_complex, zero($x_complex))
 )
 
-SUITE_ENZYME["autodiff"]["reverse"]["complex"] = @benchmarkable Enzyme.autodiff(
+SUITE["autodiff"]["reverse"]["complex"] = @benchmarkable Enzyme.autodiff(
     Enzyme.Reverse, $f_complex, Enzyme.Active, Enzyme.Duplicated($x_complex, zero($x_complex))
 )
