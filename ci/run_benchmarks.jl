1492cbf990e82fd65f47a6e1eec6109ed453e3f9
Yaraskau Kachur <yaraslau.prog.j@gmail.com>
2025-12-17 08:34:51 +0100

    fix: dynamic deps install

--- ci/run_benchmarks.jl
+++ ci/run_benchmarks.jl
@@ -2,16 +2,27 @@ using Pkg
 Pkg.activate(".")
 Pkg.instantiate()
 
-using BenchmarkTools
-
-include("../src/benchmarks_trixi.jl")
-include("../src/HistoryManager.jl")
-using .HistoryManager
-
 data_dir = length(ARGS) >= 1 ? ARGS[1] : "data"
 group_name = length(ARGS) >= 2 ? ARGS[2] : get(ENV, "BENCHMARK_GROUP", "trixi")
 commit_sha = get(ENV, "GITHUB_SHA", "")
 
-results = run(SUITE, verbose=true)
+if group_name == "trixi"
+    Pkg.add(["Trixi", "OrdinaryDiffEq"])
+    using BenchmarkTools
+    include("../src/benchmarks_trixi.jl")
+    suite = SUITE
+elseif group_name == "enzyme"
+    Pkg.add("Enzyme")
+    using BenchmarkTools
+    include("../src/benchmarks_enzyme.jl")
+    suite = SUITE_ENZYME
+else
+    error("Unknown group: $group_name")
+end
+
+include("../src/HistoryManager.jl")
+using .HistoryManager
+
+results = run(suite, verbose=true)
 
 save_benchmark_results(results, group_name; data_dir=data_dir, commit_hash=commit_sha)
